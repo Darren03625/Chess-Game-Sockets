1d5594fa663d97e5e241fbb0b481cb8b237f274e
Darren03625 <147948500+Darren03625@users.noreply.github.com>
2024-04-07 23:09:54 +0000

    test 3

--- src/hw4.c
+++ src/hw4.c
@@ -72,42 +72,91 @@ bool is_valid_pawn_move(char piece, int src_row, int src_col, int dest_row, int 
 }
 
 bool is_valid_rook_move(int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
+
+    int i;
+    if (src_row == dest_row){ // Moving horizontally
+
+        for ((dest_col > src_col) ? (i = src_col + 1) : (i = src_col - 1) ; (dest_col > src_col) ? (i < dest_col) : (i > dest_col); (dest_col > src_col) ? (i++) : (i--)){
+            if (game->chessboard[dest_row][i] != '.')
+                return false;
+        }
+    }
+    else if (src_col == dest_col){ // Moving vertically
+        
+        for ((dest_row > src_row) ? (i = src_row + 1) : (i = src_row - 1) ; (dest_row > src_row) ? (i < dest_row) : (i > dest_row); (dest_row > src_row) ? (i++) : (i--)) {
+            if (game->chessboard[i][dest_col] != '.')
+                return false;
+        }
+    }
+    else{ // Not moving horizontally or vertically
+        return false;
+    }
+
+    char destination_piece = game->chessboard[dest_row][dest_col];
+    char source_piece = game->chessboard[src_row][src_col];
+
+    if ((islower(destination_piece) && islower(source_piece)) || (isupper(destination_piece) && isupper(source_piece))) // Same color at destination piece and source piece
+        return false;
+
+
+    return true;
+}
+
+bool is_valid_knight_move(int src_row, int src_col, int dest_row, int dest_col) {
+    int rowDifference = abs(dest_row - src_row);
+    int colDifference = abs(dest_col - src_col);
+
+    return ((rowDifference == 1 && colDifference == 2 ) || (rowDifference == 2 && colDifference == 1));
+}
+
+bool is_valid_bishop_move(int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
+    int rowDifference = abs(dest_row - src_row);
+    int colDifference = abs(dest_col - src_col);
+
+    if (rowDifference != colDifference)  // Difference must be the same in order for diagonal movement
+        return false;
+
+    int rowDirection = (dest_row > src_row) ? 1 : -1; 
+    int colDirection = (dest_col > src_col) ? 1 : -1;
+
+    int startRow = src_row + rowDirection;
+    int startCol = src_col + colDirection;
+
+    while (startRow != dest_row && startCol != dest_col){
+        if (game->chessboard[startRow][startCol] != '.')
+            return false;
+        startRow += rowDirection;
+        startCol += colDirection;
+    }
+
+    char destination_piece = game->chessboard[dest_row][dest_col];
+    char source_piece = game->chessboard[src_row][src_col];
+
+    if ((islower(destination_piece) && islower(source_piece)) || (isupper(destination_piece) && isupper(source_piece))) // Same color at destination piece and source piece
+        return false;
+
+    return true;
+}
+
+bool is_valid_queen_move(int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
+    int rowDifference = abs(dest_row - src_row);
+    int colDifference = abs(dest_col - src_col);
+
+    if ((src_row == dest_row) || (src_col == dest_col) || (rowDifference == colDifference)){
+        if (is_valid_rook_move(src_row, src_col, dest_row, dest_col, game))
+            return true;
+        if (is_valid_bishop_move(src_row, src_col, dest_row, dest_col, game))
+            return true;
+    }
     
     return false;
 }
 
-bool is_valid_knight_move(int src_row, int src_col, int dest_row, int dest_col) {
-    (void)src_row;
-    (void)src_col;
-    (void)dest_row;
-    (void)dest_col;
-    return false;
-}
-
-bool is_valid_bishop_move(int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
-    (void)src_row;
-    (void)src_col;
-    (void)dest_row;
-    (void)dest_col;
-    (void)game;
-    return false;
-}
-
-bool is_valid_queen_move(int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
-    (void)src_row;
-    (void)src_col;
-    (void)dest_row;
-    (void)dest_col;
-    (void)game;
-    return false;
-}
-
 bool is_valid_king_move(int src_row, int src_col, int dest_row, int dest_col) {
-    (void)src_row;
-    (void)src_col;
-    (void)dest_row;
-    (void)dest_col;
-    return false;
+    int rowDifference = abs(dest_row - src_row);
+    int colDifference = abs(dest_col - src_col);
+
+    return (rowDifference <= 1 && colDifference <= 1);
 }
 
 bool is_valid_move(char piece, int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
